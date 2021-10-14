49ecb6c7b9386428f89c542d8b27727645461965
Anwesh43 <anweshthecool0@gmail.com>
2021-10-15 03:22:03 +0530

    adding useStyle hooks

--- src/hooks.ts
+++ src/hooks.ts
@@ -1,4 +1,4 @@
-import {useState, useEffect} from 'react'
+import {useState, useEffect, CSSProperties} from 'react'
 
 export const useAnimatedScale = () => {
     const [scale, setScale] = useState(0)
@@ -47,3 +47,32 @@ const divideScale = (
     scale : number,
     i : number,
     n : number) : number => Math.min(1 / n, maxScale(scale, i, n)) * n 
+
+
+const n : number = 5 
+
+const sinify = (scale : number) : number => Math.sin(scale * Math.PI)
+
+export const useStyle = (w : number, h : number, scale : number) => {
+    const sf : number = sinify(scale)
+    const position = 'absolute'
+    const size = Math.min(w, h) / 10
+    const width = `${size}px`
+    const height = `${size}px`
+    const background = 'indigo'
+    return {
+        blockStyle(i : number) : CSSProperties {
+            const sfj : number = divideScale(sf, i, n)
+            const top = `${i * size}px`
+            const left = `${(w / 2 - size / 2) * sfj}px`
+            return {
+                position, 
+                width, 
+                height, 
+                top, 
+                left, 
+                background 
+            }
+        }
+    }
+}
